aabd661d72191a503418c6d24ce911ecedf2ed9b
borsTiHD <bastian.j@gmx.net>
2022-07-15 18:56:08 +0200

    💄 App >> Creating starting page with new game button.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,21 @@
 <template>
-	<div class="flex flex-col">
-		<section>
-			<div>
-				new project :D
-			</div>
-		</section>
-	</div>
+    <div class="flex flex-col text-gray-900 dark:text-white">
+        <section class="flex justify-center bg-white dark:bg-body-bg p-8">
+            <AppCard class="mt-2">
+                <div class="flex flex-col">
+                    <p class="mb-2">Want to play {{ appStore.getTitle }}?</p>
+                    <NuxtLink :to="'/game'">
+                        <AppButton class="font-montserrat w-full" uppercase>New Game</AppButton>
+                    </NuxtLink>
+                </div>
+            </AppCard>
+        </section>
+    </div>
 </template>
 
 <script setup>
+import AppCard from '@/components/misc/AppCard.vue'
+import AppButton from '@/components/misc/AppButton.vue'
 import { useAppStore } from '~/store/app'
 
 // Changing title
