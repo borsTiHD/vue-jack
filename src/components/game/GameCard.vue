40abdd88c2e33a8c374f92f9e5582b5333b4e16a
borsTiHD <bastian.j@gmx.net>
2022-07-16 07:00:47 +0200

    ✨ App >> New component for showing game cards.

---
+++ src/components/game/GameCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card-face">
+        <img
+            :src="`/img/cards/${getCardName}.png`"
+            :alt="getCardName"
+            class="card-image w-full h-48 object-cover rounded-lg drop-shadow-lg"
+            loading="lazy"
+        >
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    card: {
+        type: Object,
+        required: true,
+        default: () => ({})
+    }
+})
+
+// Returns the name of the card
+// or if hidden, the name of the back card
+const getCardName = computed(() => props.card.hidden ? 'BACK' : props.card.name)
+</script>
