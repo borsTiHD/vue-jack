5ad72129ad4cc6c933673ea5ae1112fb707f0fd5
borsTiHD <bastian.j@gmx.net>
2022-07-16 07:42:55 +0200

    💄 Buttons >> Text is unselectable and added a feature for disabling button (just the visibillity).

--- src/components/misc/AppButton.vue
+++ src/components/misc/AppButton.vue
@@ -1,9 +1,9 @@
 <template>
     <button
         :class="[
-            'shadow rounded',
+            'shadow rounded select-none',
             'focus:ring-4 focus:outline-none',
-            buttonTypes[props.type],
+            buttonTypes[props.disabled ? 'disabled' : props.type],
             buttonSizes[props.size],
             props.uppercase ? 'uppercase' : 'normal-case'
         ]"
@@ -17,7 +17,8 @@
 const buttonTypes = {
     primary: 'text-white bg-primary-light dark:bg-primary hover:bg-rose-400 dark:hover:bg-rose-400 focus:ring-rose-300/60',
     secondary: 'text-gray-900 dark:text-white bg-white dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-400 focus:ring-gray-600/60',
-    success: 'text-white bg-green-600 hover:bg-green-500 focus:ring-green-300'
+    success: 'text-white bg-green-600 hover:bg-green-500 focus:ring-green-300',
+    disabled: 'text-gray-900 bg-gray-600 hover:bg-gray-500 focus:ring-gray-300'
 }
 
 const buttonSizes = {
@@ -35,6 +36,10 @@ const props = defineProps({
         default: 'default'
     },
     uppercase: {
+        type: Boolean,
+        default: false
+    },
+    disabled: {
         type: Boolean,
         default: false
     }
