e38344939993cd074c6b70ba5efa6846721ddfe9
borsTiHD <bastian.j@gmx.net>
2022-07-23 09:24:29 +0200

    ✅ Store >> Testing pages store.

---
+++ tests/store/pages.test.ts
@@ -0,0 +1,21 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { setActivePinia, createPinia } from 'pinia'
+import { usePagesStore } from '../../src/store/pages'
+
+describe('Page Store Tests', async() => {
+    beforeEach(() => {
+        // creates a fresh pinia and make it active so it's automatically picked
+        // up by any useStore() call without having to pass it to it:
+        // `useStore(pinia)`
+        setActivePinia(createPinia())
+    })
+
+    it('Check Pages', async() => {
+        const store = usePagesStore()
+        expect(store.getPages.length).toBeGreaterThanOrEqual(0) // Check if pages is not empty
+        describe.each(store.getPages)('Check every Page', (page) => {
+            expect(page).toHaveProperty('name') // Check if page has a name
+            expect(page).toHaveProperty('to') // Check if page has a to property
+        })
+    })
+})
